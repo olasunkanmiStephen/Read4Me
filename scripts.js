e2bc7b16ec27cb4e21cc4cba6b14c169aa0eec6e
Oluwaseun <adunmofestus@gmail.com>
2023-10-16 14:03:06 +0100

    all

--- scripts.js
+++ scripts.js
@@ -2,20 +2,26 @@ let speech = new SpeechSynthesisUtterance();
 
 let voices = [];
 
-let voiceselect = document.querySelector('select');
+let voiceSelect = document.querySelector('select');
 
-window.speechSynthesis.onvoiceschanged = () => {
+window.speechSynthesis.onvoiceschanged = () =>{
     voices = window.speechSynthesis.getVoices();
     speech.voice = voices[0];
 
-    voices.forEach((voice, i) => (voiceselect.options[i] = new Option(voice.name, i)));
+    voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)))
 }
 
-voiceselect.addEventListener('change', () => {
-    speech.voice = voices[voiceselect.value];
-})
+document.querySelector('#play').addEventListener('click', () =>{
+    speech.text = document.querySelector('textarea').value;
+    window.speechSynthesis.speak(speech);
+});
 
-document.querySelector('button').addEventListener('click', () => {
-   speech.text = document.querySelector('textarea').value;
-   window.speechSynthesis.speak(speech); 
-})
+document.querySelector('#pause').addEventListener('click', () =>{
+    // speech.text = document.querySelector('textarea').value;
+    window.speechSynthesis.pause(speech);
+});
+
+document.querySelector('#again').addEventListener('click', () =>{
+    // speech.text = document.querySelector('textarea').value;
+    window.speechSynthesis.resume(speech);
+});
